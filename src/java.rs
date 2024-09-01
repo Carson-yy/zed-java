5215756ef39b377bdb94bbdde9a27da16b401f4a
baopan.yang <baopan.yang@dyness-tech.com>
2024-09-01 09:27:59 +0800

    init

---
+++ src/java.rs
@@ -0,0 +1,238 @@
+use std::fs;
+
+use zed_extension_api::{
+    current_platform, download_file, latest_github_release,
+    lsp::{Completion, CompletionKind},
+    make_file_executable, register_extension, set_language_server_installation_status,
+    settings::LspSettings,
+    CodeLabel, CodeLabelSpan, DownloadedFileType, Extension, GithubReleaseOptions,
+    LanguageServerId, LanguageServerInstallationStatus, Os, Result, Worktree,
+};
+
+struct JavaExtension {
+    cached_binary_path: Option<String>,
+}
+
+impl JavaExtension {
+    fn language_server_binary_path(
+        &mut self,
+        language_server_id: &LanguageServerId,
+        worktree: &Worktree,
+    ) -> Result<String> {
+        if let Some(path) = &self.cached_binary_path {
+            if fs::metadata(path).map_or(false, |stat| stat.is_file()) {
+                return Ok(path.clone());
+            }
+        }
+
+        if let Some(path) = worktree.which("jdtls") {
+            self.cached_binary_path = Some(path.clone());
+            return Ok(path);
+        }
+
+        set_language_server_installation_status(
+            &language_server_id,
+            &LanguageServerInstallationStatus::CheckingForUpdate,
+        );
+        let release = latest_github_release(
+            "ABckh/eclipse.jdt.ls",
+            GithubReleaseOptions {
+                require_assets: true,
+                pre_release: false,
+            },
+        )?;
+
+        let (platform, _arch) = current_platform();
+        let asset_name = match platform {
+            Os::Mac | Os::Linux => "eclipse.jdt.ls.tar.gz",
+            Os::Windows => "eclipse.jdt.ls.zip",
+        };
+        let asset = release
+            .assets
+            .iter()
+            .find(|asset| asset.name == asset_name)
+            .ok_or_else(|| format!("no asset found matching {:?} \n", asset_name))?;
+
+        let version_dir = "eclipse.jdt.ls";
+        let binary_name = match platform {
+            Os::Mac | Os::Linux => "jdtls",
+            Os::Windows => "jdtls.bat",
+        };
+        let binary_path = format!("{version_dir}/bin/{binary_name}");
+
+        if !fs::metadata(&binary_path).map_or(false, |stat| stat.is_file()) {
+            set_language_server_installation_status(
+                &language_server_id,
+                &LanguageServerInstallationStatus::Downloading,
+            );
+
+            download_file(
+                &asset.download_url,
+                &version_dir,
+                match platform {
+                    Os::Mac | Os::Linux => DownloadedFileType::GzipTar,
+                    Os::Windows => DownloadedFileType::Zip,
+                },
+            )
+            .map_err(|e| format!("failed to download file: {e}"))?;
+
+            make_file_executable(&binary_path)?;
+
+            let entries =
+                fs::read_dir(".").map_err(|e| format!("failed to list working directory {e}"))?;
+            for entry in entries {
+                let entry = entry.map_err(|e| format!("failed to load directory entry {e}"))?;
+                if entry.file_name().to_str() != Some(&version_dir) {
+                    fs::remove_dir_all(&entry.path()).ok();
+                }
+            }
+        }
+
+        self.cached_binary_path = Some(binary_path.clone());
+        Ok(binary_path)
+    }
+}
+
+impl Extension for JavaExtension {
+    fn new() -> Self {
+        Self {
+            cached_binary_path: None,
+        }
+    }
+
+    fn language_server_command(
+        &mut self,
+        language_server_id: &LanguageServerId,
+        worktree: &Worktree,
+    ) -> Result<zed_extension_api::Command> {
+        Ok(zed_extension_api::Command {
+            command: self.language_server_binary_path(language_server_id, worktree)?,
+            args: Vec::new(),
+            env: Default::default(),
+        })
+    }
+
+    fn language_server_workspace_configuration(
+        &mut self,
+        language_server_id: &LanguageServerId,
+        worktree: &Worktree,
+    ) -> Result<Option<serde_json::Value>> {
+        // jdtls only accepts settings via. workspace/didChangeConfiguration, not
+        // initialization options, so pass the user's initialization options to
+        // workspace/didChangeConfiguration as well.
+        let settings = LspSettings::for_worktree(language_server_id.as_ref(), worktree)?;
+        Ok(settings.initialization_options)
+    }
+
+    fn label_for_completion(
+        &self,
+        _language_server_id: &LanguageServerId,
+        completion: Completion,
+    ) -> Option<CodeLabel> {
+        match completion.kind? {
+            CompletionKind::Method => {
+                let detail = completion.detail.unwrap();
+                let (name_and_params, return_type) = detail.split_once(" : ").unwrap();
+                let index = name_and_params.find(".").unwrap() + 1;
+                let name = &name_and_params[index..name_and_params.len()];
+                let code = format!("{return_type} {name}");
+
+                Some(CodeLabel {
+                    spans: vec![
+                        CodeLabelSpan::literal(name, Some("attribute".to_string())),
+                        CodeLabelSpan::literal(" : ", None),
+                        CodeLabelSpan::literal(return_type, Some("type".to_string())),
+                    ],
+                    filter_range: (0..name_and_params.len()).into(),
+                    code,
+                })
+            }
+            CompletionKind::Constructor => {
+                let new = "new ";
+                let code = format!("{new}{}", completion.label);
+                let name = completion.label.split('(').next()?;
+
+                Some(CodeLabel {
+                    spans: vec![CodeLabelSpan::code_range(new.len()..code.len())],
+                    filter_range: (0..name.len()).into(),
+                    code,
+                })
+            }
+            CompletionKind::Constant => {
+                let detail = completion.detail.unwrap();
+                let (name, _type) = detail.split_once(" : ").unwrap();
+                let index = name.find(".").unwrap() + 1;
+                let new_name = &name[index..name.len()];
+                let code = format!("{_type} {new_name}");
+                let highlight_name = match completion.kind? {
+                    CompletionKind::Field => Some("property".to_string()),
+                    CompletionKind::Constant => Some("constant".to_string()),
+                    _ => None,
+                };
+
+                Some(CodeLabel {
+                    spans: vec![
+                        CodeLabelSpan::literal(new_name, highlight_name),
+                        CodeLabelSpan::literal(" : ", None),
+                        CodeLabelSpan::literal(_type, None),
+                    ],
+                    filter_range: (0..name.len()).into(),
+                    code,
+                })
+            } 
+            CompletionKind::Variable | CompletionKind::Field => {
+                let (name, r#type) = completion.label.split_once(" : ")?;
+                let code = format!("{type} {name}");
+                let highlight_name = match completion.kind? {
+                    CompletionKind::Field => Some("property".to_string()),
+                    CompletionKind::Constant => Some("constant".to_string()),
+                    _ => None,
+                };
+
+                Some(CodeLabel {
+                    spans: vec![
+                        CodeLabelSpan::literal(name, highlight_name),
+                        CodeLabelSpan::literal(" : ", None),
+                        CodeLabelSpan::code_range(0..r#type.len()),
+                    ],
+                    filter_range: (0..name.len()).into(),
+                    code,
+                })
+            }
+            CompletionKind::Class | CompletionKind::Interface | CompletionKind::Enum => {
+                let name = completion.label.split(" : ").next()?;
+                let detail = completion.detail.unwrap();
+                let index = detail.rfind(".").unwrap();
+                let namespace = &detail[0..index];
+                let code = format!("{name}");
+
+                Some(CodeLabel {
+                    spans: vec![
+                        CodeLabelSpan::literal(name, Some("type".to_string())),
+                        CodeLabelSpan::literal(" : ", None), 
+                        CodeLabelSpan::literal(namespace, None),
+                    ],
+                    filter_range: (0..name.len()).into(),
+                    code,
+                })
+            }
+            CompletionKind::Keyword => Some(CodeLabel {
+                spans: vec![CodeLabelSpan::code_range(0..completion.label.len())],
+                filter_range: (0..completion.label.len()).into(),
+                code: completion.label,
+            }),
+            CompletionKind::EnumMember => {
+                let name = completion.label.split(" : ").next()?;
+
+                Some(CodeLabel {
+                    code: name.to_string(),
+                    spans: vec![CodeLabelSpan::code_range(0..name.len())],
+                    filter_range: (0..name.len()).into(),
+                })
+            }
+            _ => None,
+        }
+    }
+}
+
+register_extension!(JavaExtension);
